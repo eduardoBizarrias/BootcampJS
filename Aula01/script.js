5f904c06bafe27585e214a826e995eb79fc059e9
eduardoBizarrias <eduardo.bizarrias@gmail.com>
2023-02-19 10:09:53 -0300

    Meu primeiro script

---
+++ Aula01/script.js
@@ -0,0 +1,23 @@
+console.log("Hello World ou Olá Mundo")
+
+//let variavel dentro do escopo ou dentro de {}
+//var variavel global, funciona fora do {}
+
+//EXEMPLO let
+
+{
+    let nome = "Eduardo Bizarrias"
+    console.log(nome)
+    //enquanto o console estiver dentro dos {} ele funciona
+}
+console.log(nome)
+//se estiver fora não funciona
+
+//EXEMPLO var
+
+{
+    var nome1 = "Amanda Bizarrias"
+    //console.log(nome1)
+}
+console.log(nome1)
+//Dentro ou fora do {} a variavel funciona
